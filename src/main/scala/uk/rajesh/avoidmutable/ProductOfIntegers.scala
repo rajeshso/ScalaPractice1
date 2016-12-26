48f1a2188970794b1ebfd8127f929921940c400d
Rajesh <fun_rajesh@yahoo.com>
2016-12-26 17:32:55 +0000

    Week 2

--- src/main/scala/uk/rajesh/avoidmutable/ProductOfIntegers.scala
+++ src/main/scala/uk/rajesh/avoidmutable/ProductOfIntegers.scala
@@ -20,6 +20,20 @@ object ProductOfIntegers {
   def prod(a: Int, b : Int) : Int = mapReduce((a,b)=>a*b,1)(a,b)
   def sum(a: Int, b : Int) : Int = mapReduce((a,b)=>a+b,1)(a,b)
 
+  private class Rational(x: Int, y: Int){
+    //Use this for prod
+    require(y != 0, "Denominator must be non zero. Else you will receive a IllegalArgumentException")
+
+    //Use this during development
+    assert(y!=0, "Denominator must be non zero. Else you will receive an Assertion Error")
+
+    def this(x: Int) = this(x,1)// Always there should be a lineage to the primary constructor
+
+    def add(z: Int) = x+z
+
+    def +(z: Int) = x+z
+  }
+
   def main(args: Array[String]) {
     var p = product(1,3)
     println(s"The product function for interval 1-3 is ${p}")
@@ -32,5 +46,16 @@ object ProductOfIntegers {
     val prodCalc : Int= mapReduce((a : Int, b: Int)=>a*b,1)(1,3)
     println(s"The sum of 1-3 is "+ sumCalc + " and product is "+ prodCalc)
     println(s"The sum of 5-7 is "+ sum(5,7) + " and product is "+ prod(5,7))
+
+    //infix
+    val r1 = new Rational(1,2)
+    val r2 = new Rational(3,4)
+    val r1Plus1 = r1.add(1)
+    println("r1Plus1 = "+ r1Plus1)
+    val r1Plus2 = r1 add 1
+    println("r1Plus2 = "+ r1Plus2)
+    val r1Plus3 = r1 + 1
+    println("r1Plus3 = "+ r1Plus3)
+
   }
 }
