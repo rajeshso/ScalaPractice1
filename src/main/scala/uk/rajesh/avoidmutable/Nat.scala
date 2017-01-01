b599fca0ec4bd296dba453d27711e7e0125df620
Rajesh <fun_rajesh@yahoo.com>
2017-01-01 15:11:19 +0000

    Week 4 - Lecture 1 - Objects - Peano Numbers

---
+++ src/main/scala/uk/rajesh/avoidmutable/Nat.scala
@@ -0,0 +1,29 @@
+package uk.rajesh.avoidmutable
+//Week 4 Objects - Peano numbers
+abstract class Nat {
+  def isZero : Boolean
+  def predecessor : Nat
+  def successor : Nat = new Succ(this)
+  def + (that: Nat) : Nat
+  def - (that: Nat) : Nat
+}
+
+object Zero extends Nat {
+  override def isZero: Boolean = true
+
+  override def +(that: Nat): Nat = that
+
+  override def -(that: Nat): Nat = if (that.isZero) this else throw new IndexOutOfBoundsException("negative number reached")
+
+  override def predecessor: Nat = throw IndexOutOfBoundsException
+}
+
+class Succ(elem: Nat) extends Nat {
+  override def isZero: Boolean = false
+
+  override def +(that: Nat): Nat = new Succ(this, )
+
+  override def -(that: Nat): Nat = if (that.isZero) this else elem - that.predecessor
+
+  override def predecessor: Nat = elem
+}
